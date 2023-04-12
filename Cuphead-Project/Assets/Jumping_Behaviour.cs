c51bf3148534f47309884db3ad13b39765318da6
Mati-as <120005151+Mati-as@users.noreply.github.com>
2023-04-12 17:07:05 +0900

    chore: remove unnecessary files and code

--- Cuphead-Project/Assets/Jumping_Behaviour.cs
+++ Cuphead-Project/Assets/Jumping_Behaviour.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Jumping_Behaviour : StateMachineBehaviour
 {
+    //fileEditted
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
