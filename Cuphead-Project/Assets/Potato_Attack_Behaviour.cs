51d11ee144b20222934fa5d0e364f1dc2198008e
Mati-as <120005151+Mati-as@users.noreply.github.com>
2023-04-12 13:31:39 +0900

    fix: debug potato animation system

--- Cuphead-Project/Assets/Potato_Attack_Behaviour.cs
+++ Cuphead-Project/Assets/Potato_Attack_Behaviour.cs
@@ -9,9 +9,22 @@ public class Potato_Attack_Behaviour : StateMachineBehaviour
     readonly int POTATO_RAPID_ANIMATION_TURN =2;
     readonly int POTATO_RAPID_ANIMATION_CYCLE = 3;
 
+   
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       if(animation_turn % POTATO_RAPID_ANIMATION_CYCLE == POTATO_RAPID_ANIMATION_TURN)
+        //state 진입 시에만 속도를 제어합니다. 
+        changeAnimationSpeed(animator);
+    }
+
+    /// <summary>
+    ///보스는 2번의 턴 동안 일반속도의 공격을 하고, 
+    // 3번째 공격에서 빠른공격을 합니다. 
+    // 이부분을 제어하는 부분입니다. 
+    /// </summary>
+    /// <param name="animator"></param>
+    void changeAnimationSpeed(Animator animator)
+    {
+        if (animation_turn % POTATO_RAPID_ANIMATION_CYCLE == POTATO_RAPID_ANIMATION_TURN)
         {
             animator.speed = 2.0f;
             animation_turn++;
@@ -21,6 +34,5 @@ public class Potato_Attack_Behaviour : StateMachineBehaviour
             animator.speed = 1.2f;
             animation_turn++;
         }
-        
     }
 }
